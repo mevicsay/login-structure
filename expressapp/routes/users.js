12efd0966f46c035db9190e02bc44d1f0b0ff411
Mevicsdraw <mevics2000@gmail.com>
2018-11-27 14:05:47 +0100

    Changed

---
+++ expressapp/routes/users.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var passport = require('passport');
+
+/* GET users listing. */
+//router.get('/', function(req, res, next) {
+//  res.send('respond with a resource');
+//});
+
+router.post('/register', function(req, res, next){
+  addToDb(req, res);
+});
+
+async function addToDb(req, res){
+  var user = new User({
+    name:req.body.name,
+    username: req.body.username,
+    password: User.hashPassword(req.body.password),
+    email: req.body.email,
+    creation_dt: Date.now()
+  });
+
+  try{
+    doc = await user.save();
+    return res.status(201).json(doc);
+  }
+  catch(err){
+    return res.status(501).json(err);
+  }
+}
+
+router.post('/login', function(req, res, next){
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return res.status(501).json(err); }
+    if (!user) { return res.status(501).json(info); }
+    req.logIn(user, function(err) {
+      if (err) { return res.status(501).json(err); }
+      return res.status(200).json({ message: 'Login Success'});
+    });
+  })(req, res, next);
+});
+
+
+module.exports = router;
